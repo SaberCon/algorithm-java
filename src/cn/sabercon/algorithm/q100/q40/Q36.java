99efab087b6ca43cf53e34c45bac40f67453c800
sabercon <894876785@qq.com>
2020-03-11 22:59:49 +0800

    q36

--- src/cn/sabercon/algorithm/q100/q40/Q36.java
+++ src/cn/sabercon/algorithm/q100/q40/Q36.java
@@ -1,8 +1,80 @@
 package cn.sabercon.algorithm.q100.q40;
 
+import java.util.HashSet;
+
 /**
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * <p>
+ * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+ * <p>
+ * Example 1:
+ * Input:
+ * [
+ * ["5","3",".",".","7",".",".",".","."],
+ * ["6",".",".","1","9","5",".",".","."],
+ * [".","9","8",".",".",".",".","6","."],
+ * ["8",".",".",".","6",".",".",".","3"],
+ * ["4",".",".","8",".","3",".",".","1"],
+ * ["7",".",".",".","2",".",".",".","6"],
+ * [".","6",".",".",".",".","2","8","."],
+ * [".",".",".","4","1","9",".",".","5"],
+ * [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: true
+ * <p>
+ * Example 2:
+ * Input:
+ * [
+ *   ["8","3",".",".","7",".",".",".","."],
+ *   ["6",".",".","1","9","5",".",".","."],
+ *   [".","9","8",".",".",".",".","6","."],
+ *   ["8",".",".",".","6",".",".",".","3"],
+ *   ["4",".",".","8",".","3",".",".","1"],
+ *   ["7",".",".",".","2",".",".",".","6"],
+ *   [".","6",".",".",".",".","2","8","."],
+ *   [".",".",".","4","1","9",".",".","5"],
+ *   [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being
+ * modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ * <p>
+ * Note:
+ * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * Only the filled cells need to be validated according to the mentioned rules.
+ * The given board contain only digits 1-9 and the character '.'.
+ * The given board size is always 9x9.
+ *
  * @author ywk
  * @date 2020-03-11
  */
 public class Q36 {
+
+    public boolean isValidSudoku(char[][] board) {
+        HashSet[] rows = new HashSet[9];
+        HashSet[] cols = new HashSet[9];
+        HashSet[] boxes = new HashSet[9];
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet();
+            cols[i] = new HashSet();
+            boxes[i] = new HashSet();
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num == '.') continue;
+                int boxIndex = (i / 3) * 3 + j / 3;
+                if (rows[i].contains(num) || cols[j].contains(num) || boxes[boxIndex].contains(num)) {
+                    return false;
+                }
+                rows[i].add(num);
+                cols[j].add(num);
+                boxes[boxIndex].add(num);
+            }
+        }
+        return true;
+    }
 }
---
+++ src/cn/sabercon/algorithm/q100/q40/Q37.java
@@ -0,0 +1,8 @@
+package cn.sabercon.algorithm.q100.q40;
+
+/**
+ * @author ywk
+ * @date 2020-03-11
+ */
+public class Q37 {
+}
