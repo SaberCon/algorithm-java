28b4e1ceaae956b45ef14f6ef43f124d58f07968
sabercon <894876785@qq.com>
2020-03-14 23:32:32 +0800

    q49

--- src/cn/sabercon/algorithm/q100/q50/Q49.java
+++ src/cn/sabercon/algorithm/q100/q50/Q49.java
@@ -1,8 +1,47 @@
 package cn.sabercon.algorithm.q100.q50;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 /**
+ * Group Anagrams
+ * <p>
+ * Given an array of strings, group anagrams together.
+ * <p>
+ * Example:
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ * ["ate","eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
+ * ]
+ * <p>
+ * Note:
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ *
  * @author ywk
  * @date 2020-03-14
  */
 public class Q49 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (map.containsKey(key)) {
+                map.get(key).add(str);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(key, list);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
 }
