4ef3773aba40d24b404708fcdc953fd81afc500f
sabercon <894876785@qq.com>
2020-03-17 22:16:46 +0800

    q73

--- src/cn/sabercon/algorithm/q100/q80/Q73.java
+++ src/cn/sabercon/algorithm/q100/q80/Q73.java
@@ -1,11 +1,82 @@
 package cn.sabercon.algorithm.q100.q80;
 
 /**
+ * Set Matrix Zeroes
+ * <p>
+ * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+ * <p>
+ * Example 1:
+ * Input:
+ * [
+ *   [1,1,1],
+ *   [1,0,1],
+ *   [1,1,1]
+ * ]
+ * Output:
+ * [
+ *   [1,0,1],
+ *   [0,0,0],
+ *   [1,0,1]
+ * ]
+ * <p>
+ * Example 2:
+ * Input:
+ * [
+ *   [0,1,2,0],
+ *   [3,4,5,2],
+ *   [1,3,1,5]
+ * ]
+ * Output:
+ * [
+ *   [0,0,0,0],
+ *   [0,4,5,0],
+ *   [0,3,1,0]
+ * ]
+ * <p>
+ * Follow up:
+ * A straight forward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best solution.
+ * Could you devise a constant space solution?
  *
  * @author ywk
  * @date 2020-03-17
  */
 public class Q73 {
 
-
+    public void setZeroes(int[][] matrix) {
+        boolean firstColZero = false;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < matrix.length; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < matrix[0].length; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (firstColZero) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 }
---
+++ src/cn/sabercon/algorithm/q100/q80/Q74.java
@@ -0,0 +1,12 @@
+package cn.sabercon.algorithm.q100.q80;
+
+/**
+ *
+ *
+ * @author ywk
+ * @date 2020-03-17
+ */
+public class Q74 {
+
+
+}
