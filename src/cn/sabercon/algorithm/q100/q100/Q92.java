e8c57a9fa38e85366a5d394a8307598880f3b731
sabercon <894876785@qq.com>
2020-03-19 21:44:12 +0800

    q92

--- src/cn/sabercon/algorithm/q100/q100/Q92.java
+++ src/cn/sabercon/algorithm/q100/q100/Q92.java
@@ -1,8 +1,49 @@
 package cn.sabercon.algorithm.q100.q100;
 
 /**
+ * Reverse Linked List II
+ * <p>
+ * Reverse a linked list from position m to n. Do it in one-pass.
+ * <p>
+ * Note: 1 ≤ m ≤ n ≤ length of list.
+ * <p>
+ * Example:
+ * Input: 1->2->3->4->5->NULL, m = 2, n = 4
+ * Output: 1->4->3->2->5->NULL
+ *
  * @author ywk
  * @date 2020-03-19
  */
 public class Q92 {
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode sentry = new ListNode(0);
+        sentry.next = head;
+        int count = 1;
+        ListNode begin = sentry;
+        ListNode end = head;
+        while (count < m) {
+            begin = begin.next;
+            end = end.next;
+            count++;
+        }
+        while (count < n) {
+            ListNode temp = begin.next;
+            begin.next = end.next;
+            end.next = end.next.next;
+            begin.next.next = temp;
+            count++;
+
+        }
+        return sentry.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
 }
---
+++ src/cn/sabercon/algorithm/q100/q100/Q93.java
@@ -0,0 +1,12 @@
+package cn.sabercon.algorithm.q100.q100;
+
+/**
+ *
+ *
+ * @author ywk
+ * @date 2020-03-19
+ */
+public class Q93 {
+
+
+}
