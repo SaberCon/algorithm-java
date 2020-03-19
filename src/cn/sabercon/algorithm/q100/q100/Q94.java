a3c10368d628e473193cd2c8e54f92a151055ca3
sabercon <894876785@qq.com>
2020-03-19 22:32:29 +0800

    q94

--- src/cn/sabercon/algorithm/q100/q100/Q94.java
+++ src/cn/sabercon/algorithm/q100/q100/Q94.java
@@ -1,12 +1,70 @@
 package cn.sabercon.algorithm.q100.q100;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
- *
+ * Binary Tree Inorder Traversal
+ * <p>
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * <p>
+ * Example:
+ * Input: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * Output: [1,3,2]
+ * <p>
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
  *
  * @author ywk
  * @date 2020-03-19
  */
 public class Q94 {
 
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node.left == null && node.right == null) {
+                result.add(node.val);
+                continue;
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            stack.push(node);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            node.left = null;
+            node.right = null;
+        }
+        return result;
+    }
 
+
+    public class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+    }
 }
