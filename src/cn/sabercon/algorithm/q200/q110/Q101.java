985564138e4f99b09cd35f64e0120c10bd0e8abc
sabercon <894876785@qq.com>
2020-03-21 20:49:19 +0800

    q101

--- src/cn/sabercon/algorithm/q200/q110/Q101.java
+++ src/cn/sabercon/algorithm/q200/q110/Q101.java
@@ -1,8 +1,61 @@
 package cn.sabercon.algorithm.q200.q110;
 
 /**
+ * Symmetric Tree
+ * <p>
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * <p>
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ *  
+ * <p>
+ * But the following [1,2,2,null,3,null,3] is not:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ *  
+ * <p>
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ *
  * @author ywk
  * @date 2020-03-20
  */
 public class Q101 {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == null && right == null;
+        }
+        return left.val == right.val && check(left.left, right.right) && check(left.right, right.left);
+    }
+
+    public class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+    }
 }
