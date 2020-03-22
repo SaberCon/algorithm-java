c7374ca36b055321e641fca46587dbee71f19628
sabercon <894876785@qq.com>
2020-03-22 08:42:29 +0800

    q112

---
+++ src/cn/sabercon/algorithm/q200/q120/Q112.java
@@ -0,0 +1,55 @@
+package cn.sabercon.algorithm.q200.q120;
+
+/**
+ * Path Sum
+ *
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Given the below binary tree and sum = 22,
+ *
+ *       5
+ *      / \
+ *     4   8
+ *    /   / \
+ *   11  13  4
+ *  /  \      \
+ * 7    2      1
+ * return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ *
+ * @author ywk
+ * @date 2020-03-22
+ */
+public class Q112 {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null) {
+            if (root.right == null) {
+                return sum == root.val;
+            }
+            return hasPathSum(root.right, sum - root.val);
+        }
+        if (root.right == null) {
+            return hasPathSum(root.left, sum - root.val);
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    public static class TreeNode {
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
