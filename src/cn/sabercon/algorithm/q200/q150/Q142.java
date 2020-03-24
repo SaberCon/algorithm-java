aacc7d14ba2c39c43718d0879511da39090e4ced
sabercon <894876785@qq.com>
2020-03-24 19:15:11 +0800

    q143

---
+++ src/cn/sabercon/algorithm/q200/q150/Q142.java
@@ -0,0 +1,66 @@
+package cn.sabercon.algorithm.q200.q150;
+
+/**
+ * Linked List Cycle II
+ * <p>
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ * <p>
+ * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to.
+ * If pos is -1, then there is no cycle in the linked list.
+ * <p>
+ * Note: Do not modify the linked list.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the first node.
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ * <p>
+ * Follow-up:
+ * Can you solve it without using extra space?
+ * <p>
+ *
+ * @author ywk
+ * @date 2020-03-24
+ */
+public class Q142 {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode tortoise = head;
+        ListNode hare = head;
+        while (hare != null && hare.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            if (tortoise == hare) {
+                while (head != hare) {
+                    head = head.next;
+                    hare = hare.next;
+                }
+                return head;
+            }
+        }
+        return null;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
