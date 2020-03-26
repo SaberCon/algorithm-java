5baefe6db4fa13e07edd916a1e0452872d7d1acc
sabercon <894876785@qq.com>
2020-03-26 13:15:00 +0800

    q155

---
+++ src/cn/sabercon/algorithm/q200/q160/Q155.java
@@ -0,0 +1,63 @@
+package cn.sabercon.algorithm.q200.q160;
+
+import java.util.Stack;
+
+/**
+ * Min Stack
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ *
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ *  
+ *
+ * Example:
+ *
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ *
+ * @author ywk
+ * @date 2020-03-26
+ */
+public class Q155 {
+
+    private Stack<Integer> stack;
+
+    private Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public Q155() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || minStack.peek() >= x) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        Integer pop = stack.pop();
+        if (minStack.peek().equals(pop)) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
