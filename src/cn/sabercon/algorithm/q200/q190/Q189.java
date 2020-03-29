08bc2a920cb50f1f4fdcda6073833251127df318
sabercon <894876785@qq.com>
2020-03-29 10:42:12 +0800

    q189

---
+++ src/cn/sabercon/algorithm/q200/q190/Q189.java
@@ -0,0 +1,51 @@
+package cn.sabercon.algorithm.q200.q190;
+
+/**
+ * Rotate Array
+ * <p>
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * Example 2:
+ * <p>
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * Note:
+ * <p>
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ *
+ * @author ywk
+ * @date 2020-03-29
+ */
+public class Q189 {
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        for (int i = 0; i < nums.length / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < k / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[k - 1 - i];
+            nums[k - 1 - i] = temp;
+        }
+        for (int i = k; i < (nums.length + k) / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i + k];
+            nums[nums.length - 1 - i + k] = temp;
+        }
+    }
+}
