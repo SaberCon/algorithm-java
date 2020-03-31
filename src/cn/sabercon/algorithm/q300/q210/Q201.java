d517a08d72563a41cd000d34ee5c569664deeef1
sabercon <894876785@qq.com>
2020-03-31 12:39:29 +0800

    q202

---
+++ src/cn/sabercon/algorithm/q300/q210/Q201.java
@@ -0,0 +1,25 @@
+package cn.sabercon.algorithm.q300.q210;
+
+/**
+ * Bitwise AND of Numbers Range
+ *
+ * Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+ *
+ * Example 1:
+ *
+ * Input: [5,7]
+ * Output: 4
+ * Example 2:
+ *
+ * Input: [0,1]
+ * Output: 0
+ *
+ * @author ywk
+ * @date 2020-03-30
+ */
+public class Q201 {
+
+    public int rangeBitwiseAnd(int m, int n) {
+        return 0;
+    }
+}
---
+++ src/cn/sabercon/algorithm/q300/q210/Q202.java
@@ -0,0 +1,50 @@
+package cn.sabercon.algorithm.q300.q210;
+
+import java.util.HashSet;
+
+/**
+ * HappyNumber
+ * <p>
+ * Write an algorithm to determine if a number is "happy".
+ * <p>
+ * A happy number is a number defined by the following process:
+ * Starting with any positive integer, replace the number by the sum of the squares of its digits,
+ * and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+ * Those numbers for which this process ends in 1 are happy numbers.
+ * <p>
+ * Example: 
+ * <p>
+ * Input: 19
+ * Output: true
+ * Explanation:
+ * 1^2 + 9^2 = 82
+ * 8^2 + 2^2 = 68
+ * 6^2 + 8^2 = 100
+ * 1^2 + 0^2 + 0^2 = 1
+ *
+ * @author ywk
+ * @date 2020-03-31
+ */
+public class Q202 {
+
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        while (!set.contains(n)) {
+            if (n == 1) {
+                return true;
+            }
+            set.add(n);
+            n = getSquareSum(n);
+        }
+        return false;
+    }
+
+    private int getSquareSum(int n) {
+        int ans = 0;
+        while (n != 0) {
+            ans += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return ans;
+    }
+}
