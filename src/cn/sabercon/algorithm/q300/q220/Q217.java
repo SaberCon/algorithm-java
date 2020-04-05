c6a4bd105105b425e751c08876f7f5dff87d8a4e
sabercon <894876785@qq.com>
2020-04-05 21:54:04 +0800

    q217

---
+++ src/cn/sabercon/algorithm/q300/q220/Q217.java
@@ -0,0 +1,40 @@
+package cn.sabercon.algorithm.q300.q220;
+
+import java.util.HashSet;
+
+/**
+ * Contains Duplicate
+ *
+ * Given an array of integers, find if the array contains any duplicates.
+ *
+ * Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,1]
+ * Output: true
+ * Example 2:
+ *
+ * Input: [1,2,3,4]
+ * Output: false
+ * Example 3:
+ *
+ * Input: [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ *
+ * @author ywk
+ * @date 2020-04-05
+ */
+public class Q217 {
+
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
