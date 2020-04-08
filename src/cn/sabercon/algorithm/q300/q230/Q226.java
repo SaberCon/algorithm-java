b7557dbbc6f78d53e09ff2fdb68ff3558a3663c9
sabercon <894876785@qq.com>
2020-04-08 12:35:54 +0800

    q226

---
+++ src/cn/sabercon/algorithm/q300/q230/Q226.java
@@ -0,0 +1,59 @@
+package cn.sabercon.algorithm.q300.q230;
+
+/**
+ * Invert Binary Tree
+ *
+ * Invert a binary tree.
+ *
+ * Example:
+ *
+ * Input:
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * Output:
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * Trivia:
+ * This problem was inspired by this original tweet by Max Howell:
+ *
+ * Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+ *
+ * @author ywk
+ * @date 2020-04-08
+ */
+public class Q226 {
+
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+
+    private void invert(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invert(root.left);
+        invert(root.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
