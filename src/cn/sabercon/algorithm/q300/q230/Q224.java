6d31f590ee590de42ec8eae0bde8c5d2c4e1afe2
sabercon <894876785@qq.com>
2020-04-09 08:58:35 +0800

    q224

--- src/cn/sabercon/algorithm/q300/q230/Q224.java
+++ src/cn/sabercon/algorithm/q300/q230/Q224.java
@@ -4,21 +4,21 @@ import java.util.Stack;
 
 /**
  * Basic Calculator
- *
+ * <p>
  * Implement a basic calculator to evaluate a simple expression string.
- *
+ * <p>
  * The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: "1 + 1"
  * Output: 2
  * Example 2:
- *
+ * <p>
  * Input: " 2-1 + 2 "
  * Output: 3
  * Example 3:
- *
+ * <p>
  * Input: "(1+(4+5+2)-3)+(6+8)"
  * Output: 23
  * Note:
@@ -31,17 +31,38 @@ import java.util.Stack;
 public class Q224 {
 
     public int calculate(String s) {
-        int num = 0;
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+        Stack<Integer> nums = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == ' ') {
-                continue;
-            }
             if (Character.isDigit(c)) {
-
+                int num = c - '0';
+                while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
+                    i++;
+                    num = num * 10 + s.charAt(i) - '0';
+                }
+                if (operators.isEmpty() || operators.peek() == '(') {
+                    nums.push(num);
+                } else if (operators.peek() == '+') {
+                    operators.pop();
+                    nums.push(nums.pop() + num);
+                } else if (operators.pop() == '-') {
+                    nums.push(nums.pop() - num);
+                }
+            } else if (c == ')') {
+                operators.pop();
+                while (!operators.isEmpty() && operators.peek() != '(') {
+                    Character operator = operators.pop();
+                    if (operator == '+') {
+                        nums.push(nums.pop() + nums.pop());
+                    } else {
+                        nums.push(-nums.pop() + nums.pop());
+                    }
+                }
+            } else if (c == '(' || c == '+' || c == '-') {
+                operators.push(c);
             }
         }
-        return 0;
+        return nums.pop();
     }
 }
