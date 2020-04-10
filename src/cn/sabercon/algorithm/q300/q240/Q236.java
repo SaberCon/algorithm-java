cd2196e598a49b00b0023e4f88a06383909195b7
sabercon <894876785@qq.com>
2020-04-10 12:51:45 +0800

    q236

--- src/cn/sabercon/algorithm/q300/q240/Q236.java
+++ src/cn/sabercon/algorithm/q300/q240/Q236.java
@@ -2,27 +2,27 @@ package cn.sabercon.algorithm.q300.q240;
 
 /**
  * Lowest Common Ancestor of a Binary Search Tree
- *
+ * <p>
  * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
- *
+ * <p>
  * According to the definition of LCA on Wikipedia:
  * “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
- *
+ * <p>
  * Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
  * Output: 3
  * Explanation: The LCA of nodes 5 and 1 is 3.
  * Example 2:
- *
+ * <p>
  * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
  * Output: 5
  * Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
- *
+ * <p>
  * Note:
- *
+ * <p>
  * All of the nodes' values will be unique.
  * p and q are different and both values will exist in the binary tree.
  *
@@ -32,11 +32,23 @@ package cn.sabercon.algorithm.q300.q240;
 public class Q236 {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        while (true) {
-            if (p.val < root.val && q.val < root.val) {
-                root = root.left;
-            } else if (p.val > root.val && q.val > root.val) {
-                root = root.right;
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+        if (leftNode == null) {
+            if (rightNode == null) {
+                return null;
+            } else {
+                return rightNode;
+            }
+        } else {
+            if (rightNode == null) {
+                return leftNode;
             } else {
                 return root;
             }
