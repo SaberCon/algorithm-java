05bf2041e1a5e7899d5113871aecf92ecd1c2f4c
sabercon <894876785@qq.com>
2020-04-11 11:18:02 +0800

    q241

--- src/cn/sabercon/algorithm/q300/q250/Q241.java
+++ src/cn/sabercon/algorithm/q300/q250/Q241.java
@@ -1,5 +1,9 @@
 package cn.sabercon.algorithm.q300.q250;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -32,6 +36,62 @@ import java.util.List;
 public class Q241 {
 
     public List<Integer> diffWaysToCompute(String input) {
-        return null;
+        List<Pair<Integer, Character>> calculations = new ArrayList<>();
+        if (input.length() == 0) {
+            return Collections.emptyList();
+        }
+        int num = 0;
+        for (char c : input.toCharArray()) {
+            if (c == '+' || c == '-' || c == '*') {
+                calculations.add(new Pair<>(num, c));
+                num = 0;
+            } else {
+                num = num * 10 + c - '0';
+            }
+        }
+        calculations.add(new Pair<>(num, null));
+        return divideAndConquer(calculations, 0, calculations.size() - 1, new List[calculations.size()][calculations.size()]);
+    }
+
+    private List<Integer> divideAndConquer(List<Pair<Integer, Character>> calculations, int start, int end, List[][] cache) {
+        if (cache[start][end] != null) {
+            return cache[start][end];
+        }
+        if (start == end) {
+            cache[start][end] = Collections.singletonList(calculations.get(start).getKey());
+        } else {
+            List<Integer> ans = new ArrayList<>();
+            for (int i = start; i < end; i++) {
+                List<Integer> lefts = divideAndConquer(calculations, start, i, cache);
+                List<Integer> rights = divideAndConquer(calculations, i + 1, end, cache);
+                Character operator = calculations.get(i).getValue();
+                if (operator.equals('+')) {
+                    lefts.forEach(left -> {
+                        rights.forEach(right -> {
+                            ans.add(left + right);
+                        });
+                    });
+                } else if (operator.equals('-')) {
+                    lefts.forEach(left -> {
+                        rights.forEach(right -> {
+                            ans.add(left - right);
+                        });
+                    });
+                } else if (operator.equals('*')) {
+                    lefts.forEach(left -> {
+                        rights.forEach(right -> {
+                            ans.add(left * right);
+                        });
+                    });
+                }
+            }
+            cache[start][end] = ans;
+        }
+        return cache[start][end];
+    }
+
+    public static void main(String[] args) {
+        Q241 q241 = new Q241();
+        System.out.println(q241.diffWaysToCompute("0"));
     }
 }
