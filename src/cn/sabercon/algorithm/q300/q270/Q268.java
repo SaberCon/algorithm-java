6b8af4a47874f0a4007120852b6fc93ab5278b8c
sabercon <894876785@qq.com>
2020-04-13 13:22:38 +0800

    q268

---
+++ src/cn/sabercon/algorithm/q300/q270/Q268.java
@@ -0,0 +1,31 @@
+package cn.sabercon.algorithm.q300.q270;
+
+/**
+ * Missing Number
+ * <p>
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [3,0,1]
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ * Note:
+ * Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+ *
+ * @author ywk
+ * @date 2020-04-13
+ */
+public class Q268 {
+
+    public int missingNumber(int[] nums) {
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+}
