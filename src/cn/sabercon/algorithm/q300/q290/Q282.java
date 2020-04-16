a96c52f310e4bdcc31c1e7e3a47b752a4b49c738
sabercon <894876785@qq.com>
2020-04-16 11:34:52 +0800

    q282

--- src/cn/sabercon/algorithm/q300/q290/Q282.java
+++ src/cn/sabercon/algorithm/q300/q290/Q282.java
@@ -1,40 +1,114 @@
 package cn.sabercon.algorithm.q300.q290;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Expression Add Operators
- *
+ * <p>
  * Given a string that contains only digits 0-9 and a target value,
  * return all possibilities to add binary operators (not unary) +, -, or * between the digits so they evaluate to the target value.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: num = "123", target = 6
  * Output: ["1+2+3", "1*2*3"]
  * Example 2:
- *
+ * <p>
  * Input: num = "232", target = 8
  * Output: ["2*3+2", "2+3*2"]
  * Example 3:
- *
+ * <p>
  * Input: num = "105", target = 5
  * Output: ["1*0+5","10-5"]
  * Example 4:
- *
+ * <p>
  * Input: num = "00", target = 0
  * Output: ["0+0", "0-0", "0*0"]
  * Example 5:
- *
+ * <p>
  * Input: num = "3456237490", target = 9191
  * Output: []
+ * <p>
+ * todo This is fucking hard. I don't know how to solve it
  *
  * @author ywk
  * @date 2020-04-15
  */
 public class Q282 {
 
+    public List<String> answer;
+    public String digits;
+    public long target;
+
     public List<String> addOperators(String num, int target) {
-        return null;
+        if (num.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        this.target = target;
+        this.digits = num;
+        this.answer = new ArrayList<>();
+        recurse(0, 0, 0, 0, new ArrayList<>());
+        return this.answer;
+    }
+
+    private void recurse(int index, long previousOperand, long currentOperand, long value, List<String> ops) {
+        String nums = this.digits;
+
+        // Done processing all the digits in num
+        if (index == nums.length()) {
+
+            // If the final value == target expected AND
+            // no operand is left unprocessed
+            if (value == this.target && currentOperand == 0) {
+                StringBuilder sb = new StringBuilder();
+                ops.subList(1, ops.size()).forEach(sb::append);
+                this.answer.add(sb.toString());
+            }
+            return;
+        }
+
+        // Extending the current operand by one digit
+        currentOperand = currentOperand * 10 + Character.getNumericValue(nums.charAt(index));
+        String current_val_rep = Long.toString(currentOperand);
+        int length = nums.length();
+
+        // To avoid cases where we have 1 + 05 or 1 * 05 since 05 won't be a
+        // valid operand. Hence this check
+        if (currentOperand > 0) {
+
+            // NO OP recursion
+            recurse(index + 1, previousOperand, currentOperand, value, ops);
+        }
+
+        // ADDITION
+        ops.add("+");
+        ops.add(current_val_rep);
+        recurse(index + 1, currentOperand, 0, value + currentOperand, ops);
+        ops.remove(ops.size() - 1);
+        ops.remove(ops.size() - 1);
+
+        if (ops.size() > 0) {
+
+            // SUBTRACTION
+            ops.add("-");
+            ops.add(current_val_rep);
+            recurse(index + 1, -currentOperand, 0, value - currentOperand, ops);
+            ops.remove(ops.size() - 1);
+            ops.remove(ops.size() - 1);
+
+            // MULTIPLICATION
+            ops.add("*");
+            ops.add(current_val_rep);
+            recurse(
+                    index + 1,
+                    currentOperand * previousOperand,
+                    0,
+                    value - previousOperand + (currentOperand * previousOperand),
+                    ops);
+            ops.remove(ops.size() - 1);
+            ops.remove(ops.size() - 1);
+        }
     }
 }
