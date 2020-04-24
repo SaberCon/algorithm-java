aab01c96258750c40fa2b80db488c4958fb85d71
sabercon <894876785@qq.com>
2020-04-24 09:32:28 +0800

    q326

---
+++ src/cn/sabercon/algorithm/q400/q330/Q326.java
@@ -0,0 +1,35 @@
+package cn.sabercon.algorithm.q400.q330;
+
+/**
+ * Power of Three
+ *
+ * Given an integer, write a function to determine if it is a power of three.
+ *
+ * Example 1:
+ *
+ * Input: 27
+ * Output: true
+ * Example 2:
+ *
+ * Input: 0
+ * Output: false
+ * Example 3:
+ *
+ * Input: 9
+ * Output: true
+ * Example 4:
+ *
+ * Input: 45
+ * Output: false
+ * Follow up:
+ * Could you do it without using any loop / recursion?
+ *
+ * @author ywk
+ * @date 2020-04-24
+ */
+public class Q326 {
+
+    public boolean isPowerOfThree(int n) {
+        return Integer.toString(n, 3).matches("^10*$");
+    }
+}
