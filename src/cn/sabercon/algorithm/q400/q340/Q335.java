6d71d897a80dd26e8dbe25a885f5acd86b422612
sabercon <894876785@qq.com>
2020-04-27 12:35:50 +0800

    q335

--- src/cn/sabercon/algorithm/q400/q340/Q335.java
+++ src/cn/sabercon/algorithm/q400/q340/Q335.java
@@ -2,38 +2,38 @@ package cn.sabercon.algorithm.q400.q340;
 
 /**
  * Self Cross
- *
+ * <p>
  * You are given an array x of n positive numbers. You start at point (0,0) and moves x[0] metres to the north,
  * then x[1] metres to the west, x[2] metres to the south, x[3] metres to the east and so on.
  * In other words, after each move your direction changes counter-clockwise.
- *
+ * <p>
  * Write a one-pass algorithm with O(1) extra space to determine, if your path crosses itself, or not.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * ┌───┐
  * │   │
  * └───┼──>
  *     │
- *
+ * <p>
  * Input: [2,1,1,2]
  * Output: true
  * Example 2:
- *
+ * <p>
  * ┌──────┐
  * │      │
  * │
  * │
  * └────────────>
- *
+ * <p>
  * Input: [1,2,3,4]
  * Output: false
  * Example 3:
- *
+ * <p>
  * ┌───┐
  * │   │
  * └───┼>
- *
+ * <p>
  * Input: [1,1,1,1]
  * Output: true
  *
@@ -42,8 +42,19 @@ package cn.sabercon.algorithm.q400.q340;
  */
 public class Q335 {
 
-    public boolean isSelfCrossing(int[] x) {
+    public static boolean isSelfCrossing(int[] x) {
+        for (int i = 3; i < x.length; i++) {
+            if (x[i - 1] <= x[i - 3] && x[i] >= x[i - 2]) {
+                return true;
+            }
+            if (i > 4 && x[i - 2] > x[i - 4] && x[i - 1] < x[i - 3] && x[i] + x[i - 4] >= x[i - 2] && x[i - 1] + x[i - 5] >= x[i - 3]) {
+                return true;
+            }
+        }
+        return x.length > 4 && x[3] == x[1] && x[4] + x[0] >= x[2];
+    }
 
-        return false;
+    public static void main(String[] args) {
+        System.out.println(isSelfCrossing(new int[]{1,1,2,2,3,3,4,4,10,4,4,3,3,2,2,1,1}));
     }
 }
