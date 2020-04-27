8b0a5d49c2757888ee8534cacc23c3fd45e266e9
sabercon <894876785@qq.com>
2020-04-27 19:37:09 +0800

    q354

--- src/cn/sabercon/algorithm/q400/q360/Q354.java
+++ src/cn/sabercon/algorithm/q400/q360/Q354.java
@@ -1,5 +1,7 @@
 package cn.sabercon.algorithm.q400.q360;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -26,10 +28,32 @@ import java.util.TreeSet;
 public class Q354 {
 
     public int maxEnvelopes(int[][] envelopes) {
-        TreeMap<Integer, TreeSet<Integer>> map = new TreeMap<>();
-        for (int[] envelope : envelopes) {
-            map.computeIfAbsent(envelope[0], e -> new TreeSet<>()).add(envelope[1]);
+        if (envelopes.length == 0) {
+            return 0;
         }
-        return 0;
+        TreeMap<Integer, TreeMap<Integer, Integer>> map = new TreeMap<>();
+        for (int[] envelope : envelopes) {
+            map.computeIfAbsent(envelope[0], e -> new TreeMap<>()).put(envelope[1], 0);
+        }
+        int max = 0;
+        TreeMap<Integer, TreeMap<Integer, Integer>> tempMap = new TreeMap<>(map);
+        for (Map.Entry<Integer, TreeMap<Integer, Integer>> entry : tempMap.entrySet()) {
+            Integer currentWidth = entry.getKey();
+            for (Integer currentHeight : entry.getValue().keySet()) {
+                int currentMax = 0;
+                for (Map.Entry<Integer, TreeMap<Integer, Integer>> mapEntry : tempMap.entrySet()) {
+                    if (mapEntry.getKey() >= currentWidth) {
+                        break;
+                    }
+                    Map.Entry<Integer, Integer> lowerEntry = mapEntry.getValue().lowerEntry(currentHeight);
+                    if (lowerEntry != null) {
+                        currentMax = Math.max(currentMax, lowerEntry.getValue() + 1);
+                    }
+                }
+                map.get(currentWidth).put(currentHeight, currentMax);
+                max = Math.max(max, currentMax);
+            }
+        }
+        return max + 1;
     }
 }
