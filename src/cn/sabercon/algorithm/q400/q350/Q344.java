c9a75cc83aea5d07cb8aabff4b39219d776c5f6e
sabercon <894876785@qq.com>
2020-04-27 12:51:40 +0800

    q344

---
+++ src/cn/sabercon/algorithm/q400/q350/Q344.java
@@ -0,0 +1,34 @@
+package cn.sabercon.algorithm.q400.q350;
+
+/**
+ * Reverse String
+ *
+ * Write a function that reverses a string. The input string is given as an array of characters char[].
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * You may assume all the characters consist of printable ascii characters.
+ *
+ * Example 1:
+ *
+ * Input: ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * Example 2:
+ *
+ * Input: ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ * @author ywk
+ * @date 2020-04-27
+ */
+public class Q344 {
+
+    public void reverseString(char[] s) {
+        int length = s.length;
+        for (int i = 0; i < length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[length - i - 1];
+            s[length - i - 1] = temp;
+        }
+    }
+}
