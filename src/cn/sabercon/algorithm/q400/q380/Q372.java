ea10770d3178a28f58787bf23c5fb6ddf25608dc
sabercon <894876785@qq.com>
2020-04-28 14:39:48 +0800

    q373

---
+++ src/cn/sabercon/algorithm/q400/q380/Q372.java
@@ -0,0 +1,25 @@
+package cn.sabercon.algorithm.q400.q380;
+
+/**
+ * Super Pow
+ *
+ * Your task is to calculate a^b mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
+ *
+ * Example 1:
+ *
+ * Input: a = 2, b = [3]
+ * Output: 8
+ * Example 2:
+ *
+ * Input: a = 2, b = [1,0]
+ * Output: 1024
+ *
+ * @author ywk
+ * @date 2020-04-28
+ */
+public class Q372 {
+
+    public int superPow(int a, int[] b) {
+        return 0;
+    }
+}
