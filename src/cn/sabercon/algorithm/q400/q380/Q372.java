a9bd07543f3147fa44e73cfa6ef379044a91c5c5
sabercon <894876785@qq.com>
2020-04-28 19:48:53 +0800

    q372

--- src/cn/sabercon/algorithm/q400/q380/Q372.java
+++ src/cn/sabercon/algorithm/q400/q380/Q372.java
@@ -1,5 +1,7 @@
 package cn.sabercon.algorithm.q400.q380;
 
+import java.util.Arrays;
+
 /**
  * Super Pow
  *
@@ -19,7 +21,38 @@ package cn.sabercon.algorithm.q400.q380;
  */
 public class Q372 {
 
-    public int superPow(int a, int[] b) {
-        return 0;
+    public static int superPow(int a, int[] b) {
+        // (a * b) % k = (a % k)(b % k) % k
+        int length = b.length;
+        if (length == 0) {
+            return 1;
+        }
+        return pow(a, b[length - 1]) * pow(superPow(a, Arrays.copyOf(b, length - 1)), 10) % 1337;
+    }
+
+    private static int pow(int num, int exp) {
+        if (exp == 0) {
+            return 1;
+        }
+        if (exp == 1) {
+            return num % 1337;
+        }
+        if (exp == 2) {
+            return (num % 1337) * (num % 1337) % 1337;
+        }
+        if (exp % 2 == 1) {
+            return num % 1337 * pow(num, exp - 1) % 1337;
+        }
+        // (num^exp) % k = (num^(exp/2) * num^(exp/2)) % k = (num^(exp/2) % k) * (num^(exp/2) % k) % k
+        return pow(pow(num, exp / 2), 2);
+//        int ans = 1;
+//        for (int i = 0; i < exp; i++) {
+//            ans = num % 1337 * ans % 1337;
+//        }
+//        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pow(2, 2));
     }
 }
