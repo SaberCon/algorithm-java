0f9ebc82af34000420643cd028038c63add45c1b
sabercon <894876785@qq.com>
2020-04-29 10:14:27 +0800

    q377

---
+++ src/cn/sabercon/algorithm/q400/q380/Q377.java
@@ -0,0 +1,50 @@
+package cn.sabercon.algorithm.q400.q380;
+
+/**
+ * Combination Sum IV
+ * <p>
+ * Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
+ * <p>
+ * Example:
+ * <p>
+ * nums = [1, 2, 3]
+ * target = 4
+ * <p>
+ * The possible combination ways are:
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ * <p>
+ * Note that different sequences are counted as different combinations.
+ * <p>
+ * Therefore the output is 7. 
+ * <p>
+ * Follow up:
+ * What if negative numbers are allowed in the given array?
+ * How does it change the problem?
+ * What limitation we need to add to the question to allow negative numbers?
+ *
+ * @author ywk
+ * @date 2020-04-29
+ */
+public class Q377 {
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        // 这个值被其它状态参考，设置为 1 是合理的
+        dp[0] = 1;
+
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
