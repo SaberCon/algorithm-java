6f00b7122b4683014d904edce37cc24c7002659b
sabercon <894876785@qq.com>
2020-04-30 09:35:42 +0800

    q386

--- src/cn/sabercon/algorithm/q400/q390/Q386.java
+++ src/cn/sabercon/algorithm/q400/q390/Q386.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 /**
  * Lexicographical Numbers
- *
+ * <p>
  * Given an integer n, return 1 - n in lexicographical order.
- *
+ * <p>
  * For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
- *
+ * <p>
  * Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000.
  *
  * @author ywk
@@ -17,9 +17,24 @@ import java.util.List;
  */
 public class Q386 {
 
-    public List<Integer> lexicalOrder(int n) {
-        List<Integer> ans = new ArrayList<>(n);
+    private List<Integer> ans;
 
+    public List<Integer> lexicalOrder(int n) {
+        ans = new ArrayList<>(n);
+        for (int i = 1; i < 10; i++) {
+            dfs(n, i);
+        }
         return ans;
+    }
+
+    private void dfs(int maxValue, int num) {
+        if (num > maxValue) {
+            return;
+        }
+        ans.add(num);
+        num *= 10;
+        for (int i = 0; i < 10; i++) {
+            dfs(maxValue, num++);
+        }
     }
 }
