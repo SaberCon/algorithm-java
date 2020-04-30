8747c8c2db3a17d293b65691ab54d7acdbae948b
sabercon <894876785@qq.com>
2020-04-30 13:09:20 +0800

    q378

--- src/cn/sabercon/algorithm/q400/q380/Q378.java
+++ src/cn/sabercon/algorithm/q400/q380/Q378.java
@@ -1,5 +1,8 @@
 package cn.sabercon.algorithm.q400.q380;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Kth Smallest Element in a Sorted Matrix
  *
@@ -26,7 +29,23 @@ package cn.sabercon.algorithm.q400.q380;
 public class Q378 {
 
     public int kthSmallest(int[][] matrix, int k) {
-        
-        return 0;
+        int row = matrix.length;
+        int col = row;
+        PriorityQueue<Integer> heap = new PriorityQueue<>(k, Comparator.reverseOrder());
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (heap.size() < k) {
+                    heap.offer(matrix[i][j]);
+                } else {
+                    if (matrix[i][j] >= heap.peek()) {
+                        col = j;
+                    } else {
+                        heap.poll();
+                        heap.offer(matrix[i][j]);
+                    }
+                }
+            }
+        }
+        return heap.poll();
     }
 }
