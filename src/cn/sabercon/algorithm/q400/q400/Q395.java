aa8229a85cf63b1626f8c08bf434c81da25831c4
sabercon <894876785@qq.com>
2020-05-28 11:36:05 +0800

    q395

--- src/cn/sabercon/algorithm/q400/q400/Q395.java
+++ src/cn/sabercon/algorithm/q400/q400/Q395.java
@@ -2,26 +2,26 @@ package cn.sabercon.algorithm.q400.q400;
 
 /**
  * 395. Longest Substring with At Least K Repeating Characters
- *
+ * <p>
  * Find the length of the longest substring T of a given string (consists of lowercase letters only) such that every character in T appears no less than k times.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input:
  * s = "aaabb", k = 3
- *
+ * <p>
  * Output:
  * 3
- *
+ * <p>
  * The longest substring is "aaa", as 'a' is repeated 3 times.
  * Example 2:
- *
+ * <p>
  * Input:
  * s = "ababbc", k = 2
- *
+ * <p>
  * Output:
  * 5
- *
+ * <p>
  * The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
  *
  * @author SaberCon
@@ -29,11 +29,45 @@ package cn.sabercon.algorithm.q400.q400;
  */
 public class Q395 {
 
+    int ans;
+
+    String str;
+
+    int count;
+
     public int longestSubstring(String s, int k) {
         if (k == 0) {
             return s.length();
         }
-        return 0;
-        // todo
+        ans = 0;
+        str = s;
+        count = k;
+        findLongestSubstring(0, s.length());
+        return ans;
+    }
+
+    private void findLongestSubstring(int start, int end) {
+        if (end - start <= ans) {
+            return;
+        }
+        int[] counts = new int[26];
+        for (int i = start; i < end; i++) {
+            counts[str.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (counts[i] > 0 && counts[i] < count) {
+                int left = start;
+                char c = (char) (i + 'a');
+                for (int j = start; j < end; j++) {
+                    if (str.charAt(j) == c) {
+                        findLongestSubstring(left, j);
+                        left = j + 1;
+                    }
+                }
+                findLongestSubstring(left, end);
+                return;
+            }
+        }
+        ans = end - start;
     }
 }
---
+++ src/cn/sabercon/algorithm/q400/q400/Q399.java
@@ -0,0 +1,39 @@
+package cn.sabercon.algorithm.q400.q400;
+
+import java.util.List;
+
+/**
+ * 399. Evaluate Division
+ *
+ * Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating point number).
+ * Given some queries, return the answers. If the answer does not exist, return -1.0.
+ *
+ * Example:
+ * Given a / b = 2.0, b / c = 3.0.
+ * queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
+ * return [6.0, 0.5, -1.0, 1.0, -1.0 ].
+ *
+ * The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries ,
+ * where equations.size() == values.size(), and the values are positive. This represents the equations. Return vector<double>.
+ *
+ * According to the example above:
+ *
+ * equations = [ ["a", "b"], ["b", "c"] ],
+ * values = [2.0, 3.0],
+ * queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ].
+ *  
+ *
+ * The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/evaluate-division
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author SaberCon
+ * @date 2020-05-28
+ */
+public class Q399 {
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        return null;
+    }
+}
